fb94f67b154137e23eff7da671bee0696e3738d2
Andri Gunawan <andri_gunawan@msn.com>
2019-08-17 21:56:24 -0700

    Add ingredients property

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +9,34 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test',
-      'https://diethood.com/wp-content/uploads/2018/06/Grilled-Chicken-Thighs-Recipe.jpg'),
-    new Recipe('Another Test Recipe', 'This is simply a test',
-      'https://diethood.com/wp-content/uploads/2018/06/Grilled-Chicken-Thighs-Recipe.jpg'),
-    new Recipe('Another Test Recipe 2', 'This is simply a test',
-      'https://diethood.com/wp-content/uploads/2018/06/Grilled-Chicken-Thighs-Recipe.jpg')
+    new Recipe(
+      'Tasty Schitzel',
+      'A super-tasty Schitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]
+    ),
+    new Recipe(
+      'Big Fat Burger',
+      'What else you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ]
+    ),
+    new Recipe(
+      'Another Test Recipe 2',
+      'This is simply a test',
+      'https://diethood.com/wp-content/uploads/2018/06/Grilled-Chicken-Thighs-Recipe.jpg',
+      [
+        new Ingredient('Custom 1', 1),
+        new Ingredient('Custom 2', 1),
+        new Ingredient('Custom 3', 1)
+      ]
+    )
   ];
 
   constructor() { }
